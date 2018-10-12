e5f793dc0d73ece71a121c69fc5e4cc5de7ae6ab
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-10-12 11:06:06 -0400

    Correct the order

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 var magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
-var shakeBall = function() {
-	
-	magicFortunes.sort(function(a, b){return 0.5 - Math.random()});
-
-var button = document.getElementByTagName('button')[0];
-
-button.onclick = shakeBall;
-
 var yourFortune = document.querySelector('#fortune-wrapper p span');
 
 var fortuneTextPlaceholder = document.createTextNode('');
 yourFortune.appendChild(fortuneTextPlaceholder);
 
+var shakeBall = function() {
+	
+	magicFortunes.sort(function(a, b){return 0.5 - Math.random()});
+
+
+
+var button = document.getElementsByTagName('button')[0];
+
+
 var fortuneText = document.createTextNode(magicFortunes[0]);
 yourFortune.appendChild(fortuneText);
 
-var ourButtonSection = document.getElementByID('generator-button');
+var ourButtonSection = document.getElementById('generator-button');
 ourButtonSection.removeChild(button);
+
+
+yourFortune.classList.add('fortune-show');
+
+button.onclick = shakeBall;
+
+
